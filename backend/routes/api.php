0ba54f29ed4ea93fc7c8e943181c7e6299919018
adak <wajed@gmail.com>
2026-03-13 13:04:55 +0430

    the second changes by wajed

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,4 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TestController;
 
-Route::get('/test', [TestController::class, 'index']);
+// Route::get('/test', [TestController::class, 'index']);
+
+
+Route::get('/products', function () {
+    return response()->json([
+        "name" => "Laptop",
+        "price" => 500
+    ]);
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import { Routes, Route } from "react-router-dom"
+import { useEffect, useState } from "react";
 
 function App() {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/products")
+      .then(res => res.json())
+      .then(data => setProduct(data));
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<h1>Online Bazar</h1>} />
-    </Routes>
-  )
+    <div>
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>{product.price}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
