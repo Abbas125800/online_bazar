6a4ffc85093d4bc703de6d427a6839b48dfd7350
Sayed Nazar <sayednazar767@gmail.com>
2026-03-15 05:29:41 -0700

    no

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // ProvinceSeeder::class,
+            ProvinceSeeder::class,
             DistrictSeeder::class,
         ]);
     }
 }
+
+
